1e6c30ad4db41943eac74bd97a3a5887d80a1342
amit dixit <amitdixit.rhce@gmail.com>
2019-04-24 14:44:55 +0530

    Adding shared lib directory structure

---
+++ src/org/rbs/BitBucketHelper.groovy
@@ -0,0 +1,5 @@
+
+println "hello" //Print Hello line
+
+
+checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'URL']]])
