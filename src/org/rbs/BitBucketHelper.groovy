2f5200ef7184fa3185c933ed42f1e28002be25f2
Amitrhce <amitdixit.rhce@gmail.com>
2019-04-24 17:02:20 +0530

    Update BitBucketHelper.groovy

--- src/org/rbs/BitBucketHelper.groovy
+++ src/org/rbs/BitBucketHelper.groovy
@@ -1,12 +1,11 @@
 
 // println "hello" //Print Hello line
 
-def checkOut(repo) {
-  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'githubamit', url: 'https://github.com/Amitrhce/Jenkins-shared-library.git']]])
-println "check out from amit git repos"
-  return this
-
-}
-
-
-
+/** def checkOut(repo) {
+ *  checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'githubamit', url: 'https://github.com/Amitrhce/Jenkins-shared-library.git']]])
+ *  println "check out from amit git repos"
+ *  return this
+ *
+ * }
+ *
+ */
